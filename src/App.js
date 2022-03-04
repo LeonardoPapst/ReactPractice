535c1f1dc95ba734559636cb772fea95cdd24318
leonardopapst <leopapst@hotmail.com>
2022-03-04 16:17:17 -0300

    Adding style and organizing project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
     );
   }
--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -5,10 +5,10 @@ class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
-        <header>
-          <h3>Título</h3>
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
         </header>
-        <p>Escreva sua nota</p>
+        <p className="card-nota_texto">Escreva sua nota</p>
       </section>
     );
   }
