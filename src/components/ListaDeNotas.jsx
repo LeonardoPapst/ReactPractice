f7cb7ee05fb5335142b28a62b1efe0caf0b63de9
leonardopapst <leopapst@hotmail.com>
2022-03-04 15:10:38 -0300

    Adding JS with JSX. Added array os titles to generate a list of grades

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -5,15 +5,14 @@ class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-          <CardNota />
-        </li>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          );
+        })}
       </ul>
     );
   }
