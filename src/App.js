4bc38288a5898bf37695e51f93fe1ea8170598c2
leonardopapst <leopapst@hotmail.com>
2022-03-04 14:58:43 -0300

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
 
-function App() {
-  return (
-    <section>
-      <form>
-        <input type={"text"} placeholder="Título"/>
-        <textarea placeholder="Escreva sua nota..."/>
-        <button>Criar nota</button>
-      </form>
-      <ListaDeNotas/>
-    </section>
-  );
+class App extends Component{
+  render(){
+    return (
+      <section>
+        <FormularioCadastro/>
+        <ListaDeNotas/>
+      </section>
+    );
+  }
 }
-
 export default App;
