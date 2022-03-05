16f886c9907d525497035c1da45e60e9a18321db
leonardopapst <leopapst@hotmail.com>
2022-03-04 21:30:11 -0300

    Working with properties

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.notas = [];
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    this.notas.push(novaNota);
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
