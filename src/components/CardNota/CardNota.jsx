6ef04adf303676d0345e1de8a8f733956241b4b7
leonardopapst <leopapst@hotmail.com>
2022-03-14 22:36:06 -0300

    adding svg

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
   render() {
@@ -7,6 +8,7 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteSVG />
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
