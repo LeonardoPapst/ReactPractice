89238b4a088589c2aa0469b484cdd90773a481a8
leonardopapst <leopapst@hotmail.com>
2022-03-22 22:26:58 -0300

    addind observable architecture

--- src/dados/Categorias.js
+++ src/dados/Categorias.js
@@ -2,6 +2,14 @@ export default class Categorias{
     constructor()
     {
         this.categorias = [];
+        this._inscritos = [];
+    }
+
+    inscrever(func){
+        this._inscritos.push(func);
+    }
+    notificar(){
+        this._inscritos.forEach(func => func(this.categorias));
     }
 
     adicionarCategoria(novaCategoria){
